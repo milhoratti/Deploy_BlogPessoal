d3826906cdac404017ba9c7535c14bf31f4bea39
Victor Milhoratti <92774535+milhoratti@users.noreply.github.com>
2022-08-02 18:22:39 -0300

    fix: asterisco UserController.java

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -24,7 +24,7 @@ import com.generation.blogpessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
 	@Autowired
